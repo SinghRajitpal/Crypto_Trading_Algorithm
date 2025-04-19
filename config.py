9a1a3b5841c7ea1904e343160134a33fee34b6a1
Rajitpal Singh <singh.rajitpal@gmail.com>
2025-04-19 15:34:26 +0200

    Initial Project Setup

---
+++ config.py
@@ -0,0 +1,19 @@
+{
+    "binance_futures": {
+        "api_key": "your_api_key",
+        "api_secret": "your_api_secret",
+        "ws_base_url": "wss://stream.binance.com:9443/ws",
+    },
+
+    "binance_futures_testnet": {
+        "testnet_api_key": "74384890789851e3ce8a1b021079111d950dcc96da8ca279d129a1e987820fec",
+        "testnet_api_secret": "ba10816594558c8cd93d04e823608f408016aa15c4d632a2214ca950a50b1967",
+        "testnet_ws_base_url": "wss://testnet.binancefuture.com/ws-fapi/v1",
+    },
+
+    "symbols": [
+        "BTCUSDT",
+        ],
+    "interval": "1m",
+
+}
